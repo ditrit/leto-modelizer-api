352550af7af8d1c42b52dd2e6ba3a75bf2b446f9
Alexis <alexis.nvu@gmail.com>
2023-09-06 10:57:47 +0200

    Update configuration

--- tests/unit/services/ConfigurationService.spec.js
+++ tests/unit/services/ConfigurationService.spec.js
@@ -1,6 +1,7 @@
 import { loadConfiguration } from 'src/services/ConfigurationService.js';
 import Configuration from 'src/models/Configuration.js';
 import User from 'src/entities/User';
+import Group from 'src/entities/Group';
 
 describe('Test service: ConfigurationService', () => {
   describe('Test function: loadConfiguration', () => {
@@ -8,7 +9,7 @@ describe('Test service: ConfigurationService', () => {
       expect(loadConfiguration({ NODE_ENV: 'test' })).toEqual(new Configuration({
         mode: 'test',
         parseServer: {
-          entities: [User],
+          entities: [User, Group],
         },
       }));
     });
