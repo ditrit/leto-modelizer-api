67f78f8125cbe8ee4dfba7e3a237ec36328a15d0
Alexis <alexis.nvu@gmail.com>
2023-09-25 12:02:34 +0200

    Trigger function verification

--- src/cloud/main.js
+++ src/cloud/main.js
@@ -3,3 +3,14 @@
 /**
  * Cloud and trigger functions definition.
  */
+import { isGroupUnique } from './cloud-functions.js';
+
+Parse.Cloud.beforeSave(
+  'Group',
+  (request) => isGroupUnique(
+    request.object.existed(),
+    request.object.get('name'),
+    request.user?.getSessionToken(),
+    Parse,
+  ),
+);
