120a9ffb4803dab9ccded729f76b8d78c1041086
Vincent Moittié <moittie.vincent@gmail.com>
2024-01-22 10:57:03 +0100

    Add table to link access controls and users.

---
+++ src/main/resources/db/migration/V1_4_0__new_table_users_access_controls.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users_access_controls (
+    uac_id      SERIAL PRIMARY KEY,
+    usr_id      INTEGER REFERENCES users(usr_id) ON DELETE CASCADE NOT NULL,
+    aco_id      INTEGER REFERENCES access_controls(aco_id) ON DELETE CASCADE NOT NULL,
+    insert_date TIMESTAMP NOT NULL DEFAULT now(),
+    update_date TIMESTAMP NOT NULL DEFAULT now(),
+    UNIQUE (usr_id, aco_id)
+);
+
+COMMENT ON TABLE  users_access_controls             IS 'This table links users to access control entries, defining what access controls are applicable to each user.';
+COMMENT ON COLUMN users_access_controls.uac_id      IS 'Primary key, serial.';
+COMMENT ON COLUMN users_access_controls.usr_id      IS 'It indicates the parent access control entry for the current entry. References the aco_id in the access_controls table.';
+COMMENT ON COLUMN users_access_controls.aco_id      IS 'It represents the current access control entry in the hierarchical structure. References the aco_id in the access_controls table.';
+COMMENT ON COLUMN users_access_controls.insert_date IS 'Creation date of this row.';
+COMMENT ON COLUMN users_access_controls.update_date IS 'Last update date of this row.';
