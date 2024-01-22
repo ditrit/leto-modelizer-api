86ab40ca0dc081a82d6a574ee90a06f7d20b73fc
Vincent Moittié <moittie.vincent@gmail.com>
2024-01-22 10:56:26 +0100

    Add table for access controls

---
+++ src/main/resources/db/migration/V1_2_0__new_table_access_controls.sql
@@ -0,0 +1,23 @@
+CREATE TYPE access_control_type AS ENUM ('ROLE', 'GROUP', 'SCOPE');
+
+CREATE TABLE IF NOT EXISTS access_controls (
+    aco_id      SERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    type        access_control_type NOT NULL,
+    insert_date TIMESTAMP NOT NULL DEFAULT now(),
+    update_date TIMESTAMP NOT NULL DEFAULT now(),
+    UNIQUE (name, type)
+);
+
+COMMENT ON TYPE   access_control_type         IS 'This ENUM specifies the different categories of access control that can be applied to resources or operations. [ROLE]: Represents a specific role that users or groups can be assigned to. [GROUP]: Represents a group of users. [SCOPE]: Represents the scope or context within which access control is applied.';
+COMMENT ON TABLE  access_controls             IS 'Table to store access control records, each with a unique combination of name and type.';
+COMMENT ON COLUMN access_controls.aco_id      IS 'Primary key, serial.';
+COMMENT ON COLUMN access_controls.name        IS 'The name of the access control entry.';
+COMMENT ON COLUMN access_controls.type        IS 'The type of access control, defined by the access_control_type ENUM.';
+COMMENT ON COLUMN access_controls.insert_date IS 'Creation date of this row.';
+COMMENT ON COLUMN access_controls.update_date IS 'Last update date of this row.';
+
+INSERT INTO access_controls(aco_id, name, type) VALUES
+(1, 'Super administrator', 'ROLE'),
+(2, 'Administrator', 'ROLE'),
+(3, 'Developer', 'ROLE');
