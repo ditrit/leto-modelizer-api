fe6ae8db9017152686bb06aa0463e0d08d75b2af
Vincent Moittié <moittie.vincent@gmail.com>
2024-01-30 11:08:33 +0100

    Add sql view to get all access controls by users

---
+++ src/main/resources/db/migration/R__users_access_controls_view.sql
@@ -0,0 +1,44 @@
+-- Ensure repeatable migration is really repeated on every start
+-- ${flyway:timestamp}
+
+DROP VIEW IF EXISTS users_access_controls_view;
+
+CREATE VIEW users_access_controls_view AS
+WITH RECURSIVE all_access_controls AS (
+    SELECT
+        aco_id,
+        usr_id
+    FROM
+        users_access_controls
+    UNION
+    SELECT
+        parent,
+        usr_id
+    FROM
+        access_controls_tree
+    INNER JOIN
+        all_access_controls
+    ON
+        all_access_controls.aco_id = access_controls_tree.current
+)
+SELECT DISTINCT
+    encode(digest(concat(users.usr_id::text, access_controls.aco_id::text), 'sha256'), 'hex') AS "usa_id",
+    users.usr_id,
+    users.login,
+    users.email,
+    users.name as "user_name",
+    access_controls.aco_id,
+    access_controls.name as "access_control_name",
+    access_controls.type::text
+FROM
+    all_access_controls
+LEFT OUTER JOIN
+    access_controls
+ON
+    access_controls.aco_id = all_access_controls.aco_id
+LEFT OUTER JOIN
+    users
+ON
+    users.usr_id = all_access_controls.usr_id;
+
+COMMENT ON VIEW users_access_controls_view IS 'This view provides a consolidated look at user access controls across the system. It combines information from various tables to present a clear picture of which access controls are assigned to which users. The view employs a recursive CTE (Common Table Expression) to traverse the hierarchical structure of access controls. The final output includes unique combinations of IDs and access control details.';
