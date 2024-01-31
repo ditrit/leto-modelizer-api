f4818f6e39499e78459a3d1ba01fe5ee097eee88
Vincent Moittié <moittie.vincent@gmail.com>
2024-01-31 10:53:55 +0100

    Add mapping functions

---
+++ src/main/java/com/ditrit/letomodelizerapi/persistence/function/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides specialized implementations and extensions of the java.util.function interfaces
+ * for the LetoModelizer API's persistence layer. This package includes a variety of functional
+ * interfaces and their implementations that are specifically tailored to support database operations
+ * and data transformations within the LetoModelizer API.
+ */
+package com.ditrit.letomodelizerapi.persistence.function;
