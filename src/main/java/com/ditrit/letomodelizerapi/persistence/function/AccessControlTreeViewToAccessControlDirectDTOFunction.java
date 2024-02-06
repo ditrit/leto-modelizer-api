5dac289b54d7175a8c7bcce0676523ff53ea1dfa
Vincent Moittié <moittie.vincent@gmail.com>
2024-02-06 12:14:08 +0100

    Make function more generic for AccessControlTreeView

--- src/main/java/com/ditrit/letomodelizerapi/persistence/function/AccessControlTreeViewToAccessControlDirectDTOFunction.java
+++ src/main/java/com/ditrit/letomodelizerapi/persistence/function/AccessControlTreeViewToAccessControlDirectDTOFunction.java
@@ -2,6 +2,8 @@ package com.ditrit.letomodelizerapi.persistence.function;
 
 import com.ditrit.letomodelizerapi.model.accesscontrol.AccessControlDirectDTO;
 import com.ditrit.letomodelizerapi.persistence.model.AccessControlTreeView;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.function.Function;
 
@@ -11,15 +13,35 @@ import java.util.function.Function;
  * This function is designed to map data from a hierarchical view of access controls into a data transfer object that
  * includes information about the directness of the access control relationship.
  */
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccessControlTreeViewToAccessControlDirectDTOFunction implements Function<AccessControlTreeView,
         AccessControlDirectDTO> {
+
+    /**
+     * Indicates whether the access control information should be derived from the parent access control.
+     * When set to {@code true}, the {@link AccessControlDirectDTO} produced by the {@code apply} method will contain
+     * the ID and name from the parent access control of the provided {@link AccessControlTreeView}.
+     * When {@code false}, the DTO will instead contain the ID and name from the access control itself.
+     * <p>
+     * This field is used to dynamically determine the source of access control information based on the
+     * hierarchical relationship between access controls. It defaults to {@code true}, favoring parent access control
+     * information to populate the DTO.
+     */
+    private boolean fromParent = true;
 
     @Override
     public AccessControlDirectDTO apply(final AccessControlTreeView accessControlTreeView) {
         AccessControlDirectDTO dto = new AccessControlDirectDTO();
 
-        dto.setId(accessControlTreeView.getParentAccessControlId());
-        dto.setName(accessControlTreeView.getParentAccessControlName());
+        if (this.fromParent) {
+            dto.setId(accessControlTreeView.getParentAccessControlId());
+            dto.setName(accessControlTreeView.getParentAccessControlName());
+        } else {
+            dto.setId(accessControlTreeView.getAccessControlId());
+            dto.setName(accessControlTreeView.getAccessControlName());
+        }
+
         dto.setIsDirect(accessControlTreeView.getIsDirect());
 
         return dto;
